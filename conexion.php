4dab2c756b335e1ddcf8f956ca65920ca7377c21
Carlos Barbieri <54383068+carlosdelviento@users.noreply.github.com>
2019-11-06 00:09:49 -0300

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
- <?php
- session_start();
-
- //conexion con base de datos
-$conexion=new mysqli("localhost","root","","database") or die ("No se ha podido conectar al servidor de Base de datos ") . mysqli_connect_error();
-
-?>
