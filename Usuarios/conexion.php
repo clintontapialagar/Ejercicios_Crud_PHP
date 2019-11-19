580e38095f637310a7e8c85844fba8eb5b3e594c
Carlos Barbieri <54383068+carlosdelviento@users.noreply.github.com>
2019-11-18 21:42:10 -0300

    Update conexion.php

--- Usuarios/conexion.php
+++ Usuarios/conexion.php
@@ -2,6 +2,6 @@
  session_start();
 
  //conexion con base de datos
-$conexion = mysqli_connect("sql306.0fees.us", "0fe_24591919", "carlosdelviento123", "0fe_24591919_database") or die(mysqli_erro($mysqli));
+$conexion = mysqli_connect("localhost", "root", "", "usuarios") or die(mysqli_erro($mysqli));
 
 ?>
